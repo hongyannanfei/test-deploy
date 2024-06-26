e08e35266807221401bbaf00d0cf7bb53c2095ea
hongyannanfei <40690198+hongyannanfei@users.noreply.github.com>
2024-06-26 16:30:28 +0800

    Update github_push_docker.sh

--- github_push_docker.sh
+++ github_push_docker.sh
@@ -2,8 +2,8 @@
 
 pull_registry="docker.io" # 目标仓库
 push_registry="registry.cn-hangzhou.aliyuncs.com/custom01"
-image1=""
-image2=""
+image1="bitnami/mysql:8.0.35-debian-11-r3"
+image2="registry.cn-hangzhou.aliyuncs.com/custom01/mysql:8.0.35-debian-11-r3"
 images=($image1 $image2)
 
 for i in "${!images[@]}"

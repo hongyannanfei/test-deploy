3e1db2b1728a3ca32eb33928612cb39c9759542f
hongyannanfei <40690198+hongyannanfei@users.noreply.github.com>
2024-06-26 16:34:46 +0800

    Update github_push_docker.sh

--- github_push_docker.sh
+++ github_push_docker.sh
@@ -3,14 +3,14 @@
 pull_registry="docker.io" # 目标仓库
 push_registry="registry.cn-hangzhou.aliyuncs.com/custom01"
 image1="bitnami/mysql:8.0.35-debian-11-r3"
-image2="registry.cn-hangzhou.aliyuncs.com/custom01/mysql:8.0.35-debian-11-r3"
+image2="mysql:8.0.35-debian-11-r3"
 images=($image1 $image2)
 
 for i in "${!images[@]}"
 do
-    image=`echo ${images[$i]} | awk -F'@' '{print $1}'`
-    docker pull ${pull_registry}/${image}
-    docker tag ${pull_registry}/${image}  ${push_registry}/${image}
+    # image=`echo ${images[$i]} | awk -F'@' '{print $1}'`
+    docker pull ${pull_registry}/${image1}
+    docker tag ${pull_registry}/${image1}  ${push_registry}/${image2}
     docker images;
-    docker push ${push_registry}/${image}
+    docker push ${push_registry}/${image2}
 done
